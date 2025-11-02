325cd12f5a446c7e73f26a82b74288e82ff1aeaa
ТвоёмяаGitHub <@.com>
2025-11-02 16:42:43 +0500

    "feat(scc): Tarjan SCC algorithm + CondensationGraph"

--- src/main/java/com/aitu/Main.java
+++ src/main/java/com/aitu/Main.java
@@ -2,19 +2,29 @@ package com.aitu;
 
 import com.aitu.core.DirectedGraph;
 import com.aitu.io.CSVGraphIO;
-import com.aitu.io.JSONGraphIO;
+import com.aitu.algorithms.scc.TarjanSCC;
+import com.aitu.algorithms.scc.SCCResult;
+import com.aitu.algorithms.scc.CondensationGraph;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         String csvPath = "data/inputs/sample_graph.csv";
-        String jsonPath = "data/inputs/sample_graph.json";
-
-        DirectedGraph csvGraph = CSVGraphIO.readGraph(csvPath, 8);
+        DirectedGraph graph = CSVGraphIO.readGraph(csvPath, 8);
         System.out.println("=== GRAPH FROM CSV ===");
-        System.out.println(csvGraph);
+        System.out.println(graph);
 
-        DirectedGraph jsonGraph = JSONGraphIO.readGraph(jsonPath);
-        System.out.println("=== GRAPH FROM JSON ===");
-        System.out.println(jsonGraph);
+        TarjanSCC tarjan = new TarjanSCC();
+        SCCResult sccResult = tarjan.run(graph);
+        System.out.println("=== SCC (Tarjan) ===");
+        System.out.println("Found " + sccResult.count() + " strongly connected components.");
+        for (List<Integer> component : sccResult.getComponents()) {
+            System.out.println(component);
+        }
+
+        DirectedGraph dag = CondensationGraph.build(graph, sccResult);
+        System.out.println("=== Condensation Graph (DAG) ===");
+        System.out.println(dag);
     }
 }
---
+++ src/main/java/com/aitu/algorithms/scc/SCCResult.java
@@ -0,0 +1,20 @@
+package com.aitu.algorithms.scc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SCCResult {
+    private final List<List<Integer>> components = new ArrayList<>();
+
+    public void addComponent(List<Integer> component) {
+        components.add(component);
+    }
+
+    public List<List<Integer>> getComponents() {
+        return components;
+    }
+
+    public int count() {
+        return components.size();
+    }
+}
